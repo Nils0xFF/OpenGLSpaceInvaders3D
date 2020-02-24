88cc8bd7a4a6cf68d7bf8a8797f0afd57284e589
DontMash <soeren@maschmann.online>
2020-02-24 07:32:42 +0100

    Documentation

--- src/BulletController.cpp
+++ src/BulletController.cpp
@@ -1,1 +1,28 @@
 #include "BulletController.h"
+
+void BulletController::Start() {
+	// If the bullet-controller is attached to a gameObject, which contains a ParticleGenerator
+	// it will emit particles as long as the bullet exists
+	if (gameObject) {
+		ParticleGenerator* generator = gameObject->getComponentByType<ParticleGenerator>();
+		if (generator) {
+			generator->setEmiting(true);
+		}
+	}
+}
+
+void BulletController::Update(float deltaTime) {
+	// Translates the bullet by its direction & speed every frame
+	if (gameObject->getTransform().translation().Z < -GameSettings::WORLD_DEPTH || gameObject->getTransform().translation().Z > 2) {
+		gameObject->Destroy();
+	}
+	gameObject->translate(direction * speed * deltaTime);
+}
+
+void BulletController::onCollision(GameObject* other) {
+	// If a bullet hits another bullet, both will be destroyed
+	if (other->getTag() == Tag::EnemyBullet || other->getTag() == Tag::PlayerBullet) {
+		this->Destroy();
+		other->Destroy();
+	}
+}
--- src/Component.h
+++ src/Component.h
@@ -13,17 +13,24 @@ public:
 	Component(GameObject* gameObject):gameObject(gameObject) {};
 	virtual ~Component() { gameObject = NULL; }
 
+	// Called before Start() as first method in every component
 	virtual void Init() {};
+	// Called after Init() as second method in every component
 	virtual void Start() {};
+	// Called every frame with corresponding deltaTime
 	virtual void Update(float deltaTime) = 0;
+	// Called every frame
 	virtual void Draw() {};
+	// Used to mark Component for "garbage collection"
 	virtual void Destroy() {};
+	// Callback for changed transform
 	virtual void updateTransform() {};
 
+	// Callback for collisions
 	virtual void onCollision(GameObject* other) {};
-
+	// Used for deep copies of components (prefabs)
 	virtual Component* clone() = 0;
-
+	
 	virtual void setGameObject(GameObject* go) { this->gameObject = go; }
 };
 
